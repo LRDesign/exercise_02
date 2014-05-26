f509710e534f8f8797d827f1b9231afd6448a059
Evan Dorn <evan@lrdesign.com>
2014-05-26 12:07:27 -0700

    Fix syntax error in spec

--- number_tool_spec.rb
+++ number_tool_spec.rb
@@ -30,7 +30,7 @@ describe NumberTool do
     it "When I create one of them the instance count should increase by one" do
       expect do
         NumberTool.new(1,1)
-      end.to change { NumberTool.instance_count }.by)(1)
+      end.to change { NumberTool.instance_count }.by(1)
     end
 
     it "When I create three of them the instance count should increase by three" do
@@ -38,7 +38,7 @@ describe NumberTool do
         NumberTool.new(1,1)
         NumberTool.new(1,1)
         NumberTool.new(1,1)
-      end.to change { NumberTool.instance_count }.by)(3)
+      end.to change { NumberTool.instance_count }.by(3)
     end
   end
 
